48e891fb40027f510dba5567dddedcb09646d5c0
gabrielnidzgorski <justakkio@gmail.com>
2026-01-21 16:07:44 +0100

    Fix

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-a = float ( input (’podaj A: ’) )
-b = float ( input (’podaj B: ’) )
-operacja = input (’podaj operacje : ’)
+a = float ( input ('podaj A: ') )
+b = float ( input ('podaj B: ') )
+operacja = input ('podaj operacje : ')
 
-if operacja == ’+’:
-print ( f’{a} + {b} = {a+b}’)
-elif operacja == ’ - ’:
-print ( f ’{ a } - { b } = {a - b } ’)
+if operacja == '+':
+    print ( f'{a} + {b} = {a+b}')
+elif operacja == ' - ':
+    print ( f'{ a } - { b } = {a - b } ')
